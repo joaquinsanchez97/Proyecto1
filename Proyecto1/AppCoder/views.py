570e3f1f76aa654e1b1d501d6679969b91a94645
Joaquín Sanchez <joaquinsanchez97@hotmail.com>
2023-06-13 12:02:33 +0200

    Primer parte playground coder

--- Proyecto1/AppCoder/views.py
+++ Proyecto1/AppCoder/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return HttpResponse("Vista profesores")
+
+def estudiantes(request):
+    return HttpResponse("Vista estudiantes")
+
+def entregables(request):
+    return HttpResponse("Vista entregables")
+
