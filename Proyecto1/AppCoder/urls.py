f82b7657635e7e7ce12741a30f56bbf81895acb8
Joaquín Sanchez <joaquinsanchez97@hotmail.com>
2023-06-25 21:26:46 +0200

    Insert and select forms

--- Proyecto1/AppCoder/urls.py
+++ Proyecto1/AppCoder/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from AppCoder.views import inicio, cursos, entregables, estudiantes, profesores
+from AppCoder.views import * #inicio, cursos, entregables, estudiantes, profesores, setEstudiantes, getEstudiantes, buscarEstudiante
 
 urlpatterns = [
     path('inicio/', inicio),
     path('cursos/', cursos, name="Cursos"),
     path('entregables/', entregables, name="Entregables"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
-    path('profesores/', profesores, name="Profesores")
+    path('profesores/', profesores, name="Profesores"),
+    path('setEstudiante/', setEstudiantes, name="setEstudiante"),
+    path('getEstudiante/', getEstudiantes, name="getEstudiante"),
+    path('buscarEstudiante/', buscarEstudiante, name="buscarEstudiante"),
+
 ]
 
